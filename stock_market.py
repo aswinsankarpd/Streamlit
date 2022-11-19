0d134b4b7116693029297fd6dfc932d677303912
Aswin Sankar P D <you@example.com>
2022-11-19 19:26:54 +0530

    first commit

---
+++ stock_market.py
@@ -0,0 +1,14 @@
+import yfinance as yf
+import streamlit as st 
+import pandas as pd 
+
+st.write("STOCK MARKET APP")
+
+tickerSymbol = "GOOGL"
+
+tickerData = yf.Ticker(tickerSymbol)
+
+tickerDf = tickerData.history(period="id", start='2010-5-31',end='2020-5-31')
+
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
